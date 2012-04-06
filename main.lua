f37add5aea2a6f1ba9798dc621a20fa5a8676b84
Rafal Dittwald <rafal.dittwald@gmail.com>
2012-04-06 10:56:30 -0400

    use constants for pos, update image

--- main.lua
+++ main.lua
@@ -2,9 +2,12 @@ print ( "hello" )
 
 MOAISim.openWindow ( "Penyo Restaurant", 480, 320 )
 
+VIEW_W = 480
+VIEW_H = 320
+
 viewport = MOAIViewport.new ()
 viewport:setScale ( 480, 320 )
-viewport:setSize ( 480, 320 )
+viewport:setSize ( VIEW_W, VIEW_H )
 
 layer = MOAILayer2D.new ()
 layer:setViewport ( viewport )
@@ -14,7 +17,7 @@ MOAISim.pushRenderPass ( layer )
 
 bgGfx = MOAIGfxQuad2D.new ()
 bgGfx:setTexture ( "assets/images/background.png" )
-bgGfx:setRect ( -240, -160, 240, 160 )
+bgGfx:setRect ( -VIEW_W/2, -VIEW_H/2, VIEW_W/2, VIEW_H/2 )
    
 base = MOAIProp2D.new ()
 base:setDeck ( bgGfx )
@@ -24,19 +27,26 @@ layer:insertProp ( base )
 
 
 -- insert order
+ORDER_W = 128
+ORDER_H = 160
 
 orderGfx = MOAIGfxQuad2D.new ()
 orderGfx:setTexture ( "assets/images/order.png" )
-orderGfx:setRect ( -75, -75, 75, 75 )
+orderGfx:setRect ( -ORDER_W/2, -ORDER_H/2, ORDER_W/2, ORDER_H/2 )
 
-function makeRocket ()
+function makeOrder ()
+  local start_x = VIEW_W+ORDER_W/2
+  local start_y = 70
+  local end_x = -VIEW_W/2+ORDER_W/2
+  local end_y = start_y
+
   local order = MOAIProp2D.new ()
   order:setDeck ( orderGfx )
-  order:setLoc (240+75,75)
+  order:setLoc ( start_x, start_y )
   layer:insertProp ( order )
 
   function order:main ()
-    MOAIThread.blockOnAction ( self:seekLoc ( -240+75, 75, 5.0, MOAIEaseType.LINEAR ))
+    MOAIThread.blockOnAction ( self:seekLoc ( end_x, end_y, 5.0, MOAIEaseType.LINEAR ))
   end
 
   order.thread = MOAIThread.new ()
@@ -44,4 +54,4 @@ function makeRocket ()
 
 end
 
-makeRocket ()
+makeOrder ()
